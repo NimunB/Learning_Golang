3d294a9adbbf831be5bdf6c5ec17b0dbe26978f2
Nimun Bajwa <nimun2001@gmail.com>
2023-05-09 14:08:49 -0400

    5. Return greetings for multiple people using Key/Value Pairs in a Map

--- Tutorials/2_goModule/greetings/greetings.go
+++ Tutorials/2_goModule/greetings/greetings.go
@@ -16,6 +16,26 @@ func Hello(name string) (string, error) { // Change the function so that it retu
     message := fmt.Sprintf(randomFormat(), name) // shortcut for declaring and initializing a variable in one line
     return message, nil // Add nil (meaning no error) as a second value in the successful return. That way, the caller can see that the function succeeded.
 }
+
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) { // parameter is a slice of names rather than a single name. Also, you change one of its return types from a string to a map so you can return names mapped to greeting messages.
+	// A map to associate names with messages.
+	messages := make(map[string]string) // make(map[key-type]value-type) - Create a messages map to associate each of the received names (as a key) with a generated message
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names { // range returns two values: the index of the current item in the loop and a copy of the item's value. You don't need the index, so you use the Go blank identifier (an underscore) to ignore it.
+			message, err := Hello(name)
+			if err != nil {
+					return nil, err
+			}
+			// In the map, associate the retrieved message with
+			// the name.
+			messages[name] = message
+	}
+	return messages, nil
+}
+
 // init sets initial values for variables used in the function.
 func init() {
 	rand.Seed(time.Now().UnixNano()) // using time to set the seed of rand
--- Tutorials/2_goModule/hello/hello.go
+++ Tutorials/2_goModule/hello/hello.go
@@ -13,10 +13,13 @@ func main() { // Declare a main package. In Go, code executed as an application 
     log.SetPrefix("greetings: ") // Configure the log package to print the command name ("greetings: ") at the start of its log messages
     log.SetFlags(0) // without a time stamp or source file information.
 
-    // ----Request greeting message------
+		// A slice of names.
+    names := []string{"Nimun", "Bruno", "Kate"} // Create a names variable as a slice type holding three names.
+
+    // ----Request greeting messages for the names------
 
 		// Check for error
-		message, err := greetings.Hello("Nimmy") // Assign both of the Hello return values, including the error, to variables.
+		messages, err := greetings.Hellos(names) // Assign both of the Hello return values, including the error, to variables.
 
     // If an error was returned, print it to the console and
     // exit the program.
@@ -24,7 +27,6 @@ func main() { // Declare a main package. In Go, code executed as an application 
         log.Fatal(err)
     }
 
-    // If no error was returned, print the returned message
-    // to the console.
-    fmt.Println(message)
+    // If no error was returned, print the returned map of messages to the console.
+    fmt.Println(messages)
 }
