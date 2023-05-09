9180017e53cfa63e856bc4550cd3a625cf6fb431
Nimun Bajwa <nimun2001@gmail.com>
2023-05-09 13:27:48 -0400

    3. Return and handle an error

--- Tutorials/2_goModule/greetings/greetings.go
+++ Tutorials/2_goModule/greetings/greetings.go
@@ -1,10 +1,16 @@
 package greetings // Declare a greetings package to collect related functions.
 
-import "fmt"
-
+import (
+	"fmt"
+	"errors"
+)
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) { // Change the function so that it returns two values: a string and an error. Your caller will check the second value to see if an error occurred. 
+		// If no name was given, return an error with a message.
+    if name == "" {
+			return "", errors.New("empty name")
+		}
     // Return a greeting that embeds the name in a message.
     message := fmt.Sprintf("Hi, %v. Welcome!", name) // shortcut for declaring and initializing a variable in one line
-    return message
+    return message, nil // Add nil (meaning no error) as a second value in the successful return. That way, the caller can see that the function succeeded.
 }
--- Tutorials/2_goModule/hello/hello.go
+++ Tutorials/2_goModule/hello/hello.go
@@ -2,12 +2,29 @@ package main
 
 import (
     "fmt"
-
+		"log" // for logging
     "example.com/greetings" // importing greeting package
 )
 
 func main() { // Declare a main package. In Go, code executed as an application must be in a main package
-    // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
+		// Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ") // Configure the log package to print the command name ("greetings: ") at the start of its log messages
+    log.SetFlags(0) // without a time stamp or source file information.
+
+    // ----Request greeting message------
+
+		// Check for error
+		message, err := greetings.Hello("") // Assign both of the Hello return values, including the error, to variables.
+
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // If no error was returned, print the returned message
+    // to the console.
     fmt.Println(message)
 }
