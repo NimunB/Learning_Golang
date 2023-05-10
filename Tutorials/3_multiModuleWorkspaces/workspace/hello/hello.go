c6fee142232fad993d73f7628c42231c7703ed4b
Nimun Bajwa <nimun2001@gmail.com>
2023-05-10 11:19:41 -0400

    created toUpper function in example and called it in hello.go

--- Tutorials/3_multiModuleWorkspaces/workspace/hello/hello.go
+++ Tutorials/3_multiModuleWorkspaces/workspace/hello/hello.go
@@ -2,9 +2,9 @@ package main
 
 import (
     "fmt"
-    "golang.org/x/example/stringutil"
+    "golang.org/x/example/stringutil" // resolves the golang.org/x/example import using the go.work file.
 )
 
 func main() {
-    fmt.Println(stringutil.Reverse("Hello"))
-}git
+    fmt.Println(stringutil.ToUpper("Hello")) // use toUpper in another module in workspace. eliminates need for multiple replaces in go.mod
+}
